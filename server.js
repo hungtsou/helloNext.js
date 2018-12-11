8614402bb63e8e3c9239edad7af9104381960f5f
hungtsou <hungtsougg@gmail.com>
2018-12-11 05:16:15 -0600

    Integration test

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.prepare()
 
   server.listen(3000, (err) => {
     if (err) throw err
-    console.log('> Ready on http://localhost:3000')
+    console.log('> Test Ready on http://localhost:3000')
   })
 })
 .catch((ex) => {
